f76116e6105bca31d7ff6c0ed05cffe5da514b11
Wisdom Ikechi <>
2025-09-09 09:24:33 +0100

    Initial Commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,51 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+        Random random = new Random();
+
+        while (playAgain.ToLower() == "yes")
+        {
+            // Generating random number between 1 and 100
+            int magicNumber = random.Next(1, 101);
+            int guess = -1;
+
+            Console.WriteLine("I have picked a number between 1 and 100. Can you guess it?");
+
+            // Loop for correct guess
+            while (guess != magicNumber)
+            {
+                Console.Write("Enter your guess: ");
+                string input = Console.ReadLine();
+
+                // Validate input
+                if (!int.TryParse(input, out guess))
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    continue;
+                }
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher!");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower!");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed it!");
+                }
+            }
+
+            // Ask if the user wants to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
+
+        Console.WriteLine("Thanks for playing! Goodbye!");
     }
 }

77775d7150f7b493361645feb03704fb07394a7e
Wisdom Ikechi <>
2025-09-14 12:52:47 +0100

    initial commit

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
-    }
+          // First job
+            Job job1 = new Job();
+            job1._jobTitle = "Junior Software Engineer";
+            job1._company = "HiiT PLC";
+            job1._startYear = 2020;
+            job1._endYear = 2022;
+
+            // Second job
+            Job job2 = new Job();
+            job2._jobTitle = "DevOps Engineer";
+            job2._company = "Google";
+            job2._startYear = 2022;
+            job2._endYear = 2025;
+
+            // Create Resume
+            Resume myResume = new Resume();
+            myResume._name = "Wisdom Ikechi";
+            myResume._jobs.Add(job1);
+            myResume._jobs.Add(job2);
+
+            // Display Resume
+            myResume.DisplayResume();
+     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+public class Resume
+{
+    public string _name;
+
+    // list of jobs
+    public List<Job> _jobs = new List<Job>();
+
+    // Resume details
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
+}
